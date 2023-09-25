ff29ee51f565404c3f5fe1ee0c8cdc5c8072f169
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2023-09-25 12:21:41 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program {
+    static void Main() {
+        Circle circle = new Circle();
+        circle.CualEsMiRadio();
+        circle.CalculaTuPerimetro();
+        circle.CalculaTuArea();
+    }
+}
+
+class Circle {
+    float radio_size;
+     public void CualEsMiRadio() {
+        Console.WriteLine("Escribe el radio del circulo");
+        radio_size = int.Parse(Console.ReadLine());
+     }
+
+
+     public void CalculaTuPerimetro() {
+        Console.WriteLine("\nMi perimetro es " + 2 * Math.PI * radio_size);
+     }
+
+     public void CalculaTuArea() {
+        Console.WriteLine("Mi area es " + Math.PI * Math.Pow(radio_size, 2));
+     }
+}
